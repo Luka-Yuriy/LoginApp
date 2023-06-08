fbe19f0cc0e44798f00566872fabe37129f494ba
Luka Yurii <lukayurii.work@gmail.com>
2023-06-08 20:02:02 +0500

    Setup botton function

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var userName = "User"
+    private var userPassword = "Password"
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var userPasswordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func logInButtonPressed(_ sender: Any) {
+        if userNameTF.text != userName || userPasswordTF.text != userPassword {
+            showAlert(whith: "Invalid login or password",
+                      and: "Please, enter correct login and password")
+        }
+    }
+    
+    @IBAction func fogotUserNameButtonPressed(_ sender: Any) {
+        showAlert(whith: "Oops!", and: "Your name is User 😋")
+    }
+    
+    @IBAction func fogotPasswordButtonPressed(_ sender: Any) {
+        showAlert(whith: "Oops!", and: "Your password is Password 😋")
+    }
 }
+
+
+// MARK: - UIAlertController
+extension ViewController {
+    private func showAlert(whith title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
 
